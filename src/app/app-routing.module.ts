0c6d26559e5552587724457027843845fc8c646a
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2026-01-22 10:06:45 +0100

    Integrate Bootstrap and redesign coches component UI
    Added Bootstrap and Bootstrap Icons to the project for improved styling. Refactored coches.component.html and coches.component.css to use Bootstrap-inspired classes, enhanced responsive layout, and modernized the form and card UI for better usability and appearance. Updated app-routing to set CochesComponent as the default route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CochesComponent } from './modules/coches/coches.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/coches', pathMatch: 'full' },
+  { path: '', component: CochesComponent},
   { path: 'coches', loadChildren: () => import('./modules/coches/coches.module').then(m => m.CochesModule) }, 
   { path: 'ofertas', loadChildren: () => import('./modules/ofertas/ofertas.module').then(m => m.OfertasModule) }];
 
